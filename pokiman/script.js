b9651b953900ad01d848cd68ac5ac9478c5504db
JuanPMCR <jpmc2907@gmail.com>
2026-04-15 08:56:38 -0600

    agregue los movimientos con detalles

--- pokiman/script.js
+++ pokiman/script.js
@@ -1,3 +1,5 @@
+let currentPokemon = null;
+
 async function fetchingPokemon(pokemon) {
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon).then(response => {
         if (!response.ok) {
@@ -13,11 +15,12 @@ async function fetchingPokemon(pokemon) {
 }
 
 function searchPokemon() {
-    let busq = document.getElementById("buscarPokemon").value;
+    let busq = document.getElementById("buscarPokemon").value.toLowerCase();
     fetchingPokemon(busq);
 }
 
 function changePokemon(pokemon) {
+    currentPokemon = pokemon;
     let name = document.getElementById("nomPokemon");
     let type = document.getElementById("typePokemon");
     let wei = document.getElementById("weightPokemon");
@@ -30,11 +33,58 @@ function changePokemon(pokemon) {
 
     name.innerHTML = pokemon.name;
     type.innerHTML = pokemon.types.map(t => t.type.name).join("<br>");
-    height.innerHTML = pokemon.height + " mt";
-    wei.innerHTML = pokemon.weight + " kg";
+    height.innerHTML = pokemon.height + " ft";
+    wei.innerHTML = pokemon.weight + " lbs";
     id.innerHTML = pokemon.id;
     ability.innerHTML = pokemon.abilities.map(abil => abil.ability.name).join(" | ");
     moves.innerHTML = pokemon.moves.map(mov => mov.move.name).join(" | ");
     img.src = pokemon.sprites.front_default;
     stats.innerHTML = pokemon.stats.map(stat => stat.stat.name + ": " + stat.base_stat).join("<br>");
+}
+
+//movimientos
+
+async function fetchingPokemonMov(moves) {
+    fetch("https://pokeapi.co/api/v2/move/" + moves).then(response => {
+        if (!response.ok) {
+        throw new Error("Error");
+    }
+    return response.json();
+    }).then(data => {
+        console.log(data);
+        changeMoves(data);
+    }).catch(error => {
+        console.error("Error fetching", error);
+    });
+}
+
+function searchMoves() {
+    if (!currentPokemon) {
+        document.getElementById("movesDetails").innerHTML = "nigga";
+        return;
+    }
+
+    let busq = document.getElementById("buscarMovimiento").value.toLowerCase();
+    if (!busq) {
+        document.getElementById("movesDetails").innerHTML = "Ingresa un movimiento para buscar.";
+        return;
+    }
+
+    const puedeAprender = currentPokemon.moves.some(mov => mov.move.name.toLowerCase() == busq);
+
+    if (puedeAprender) {
+        fetchingPokemonMov(busq);
+    } else {
+        document.getElementById("movesDetails").innerHTML = "Este pokemon no aprende ese movimiento.";
+    }
+}
+
+function changeMoves(moves) {
+    let moveDetails = document.getElementById("movesDetails");
+    moveDetails.innerHTML = "Nombre: " + moves.name + "<br>" +
+                            "Tipo: " + moves.type.name + "<br>" +
+                            "Poder: " + moves.power + "<br>" +
+                            "PP: " + moves.pp + "<br>" +
+                            "prioridad: " + moves.priority + "<br>" +
+                            "Precision: " + moves.accuracy;
 }
